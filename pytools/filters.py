ab2b425d8bf9b9383934979c9eeec2aa14fc6737
DrGFreeman <DrGFreeman@users.noreply.github.com>
2018-09-20 23:43:29 -0400

    Removed useless try/except clauses

--- pytools/filters.py
+++ pytools/filters.py
@@ -35,13 +35,10 @@ class Filter1D:
         """Class constructor. maxSize argument defines the size (number of data
         points) of the signal to be kept. maxSize must be an odd integer >= 3.
         """
-        try:
-            if maxSize % 2 == 1 and maxSize >= 3:
-                self._maxSize = maxSize
-            else:
-                raise ValueError("maxSize must be an odd integer >= 3")
-        except ValueError:
-            raise
+        if maxSize % 2 == 1 and maxSize >= 3:
+            self._maxSize = maxSize
+        else:
+            raise ValueError("maxSize must be an odd integer >= 3")
         self._data = np.ndarray(0)
 
     def addDataPoint(self, dataPoint):
@@ -51,7 +48,7 @@ class Filter1D:
         a numpy one dimensional array."""
         ##  Append new data point(s) to end of array
         self._data = np.insert(self._data, self._data.size, dataPoint)
-        ##  Trim begining begining of array if longer than maxSize
+        ##  Trim begining of array if longer than maxSize
         if self._data.size > self._maxSize:
             self._data = self._data[self._data.size - self._maxSize:]
 
@@ -68,31 +65,25 @@ class Filter1D:
         equals windowSize. If windowSize is not specified, is set to 0 or is
         greater than maxSize, windowSize will be automatically set to maxSize
         and the mean of the entire data array will be returned."""
-        try:
-            if self._data.size == 0:
-                raise RuntimeError("Filter1D data is empty. Call Filter1D.addDataPoint() to add data prior calling Filter1D.getMean().")
-            if type(windowSize) is int:
-                if windowSize <= 0 or windowSize > self._maxSize:
-                    windowSize = self._maxSize
-                return np.mean(self._data[-windowSize:])
-            else:
-                raise TypeError("windowSize must be an integer")
-        except TypeError or RuntimeError:
-            raise
+        if self._data.size == 0:
+            raise RuntimeError("Filter1D data is empty. Call Filter1D.addDataPoint() to add data prior calling Filter1D.getMean().")
+        if type(windowSize) is int:
+            if windowSize <= 0 or windowSize > self._maxSize:
+                windowSize = self._maxSize
+            return np.mean(self._data[-windowSize:])
+        else:
+            raise TypeError("windowSize must be an integer")
 
     def getMedian(self, windowSize=0):
         """Returns the median of the last n points from the data array where n
         equals windowSize. windowSize must be an odd integer. If windowSize
         is not specified or is set to 0, windowSize will be automatically set
         to maxSize and the median of the entire data array will be returned."""
-        try:
-            if self._data.size == 0:
-                raise RuntimeError("Filter1D data is empty. Call Filter1D.addDataPoint() to add data prior calling Filter1D.getMedian().")
-            if windowSize <= 0:
-                windowSize = self._maxSize
-            if windowSize % 2 == 1 and windowSize <= self._maxSize:
-                return np.median(self._data[-windowSize:])
-            else:
-                raise ValueError("windowSize must be an odd integer <= maxSize")
-        except ValueError:
-            raise
+        if self._data.size == 0:
+            raise RuntimeError("Filter1D data is empty. Call Filter1D.addDataPoint() to add data prior calling Filter1D.getMedian().")
+        if windowSize <= 0:
+            windowSize = self._maxSize
+        if windowSize % 2 == 1 and windowSize <= self._maxSize:
+            return np.median(self._data[-windowSize:])
+        else:
+            raise ValueError("windowSize must be an odd integer <= maxSize")
